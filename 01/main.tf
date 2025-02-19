3870ba5b7af309523f1116e9b0fa48b893462885
Darshan <uba@Darshan.local>
2025-02-19 00:23:49 -0600

    local variable - for expressions

--- 01/main.tf
+++ 01/main.tf
@@ -20,11 +20,15 @@ variable "ami" {
   
 }
 
+locals {
+  tag_name = "test-ec2-02"
+}
+
 resource "aws_instance" "app_server" {
   ami           = var.ami 
   instance_type = var.instance_type
 
   tags = {
-    Name = "Test01"
+    Name = "${local.tag_name}"
   }
 }
